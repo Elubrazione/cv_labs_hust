64f9448a6e9109a267bb118b25dcffb1013d3acd
jelly <1355532189@qq.com>
2023-04-01 17:38:50 +0800

    done

--- lab2/draw_fig.py
+++ lab2/draw_fig.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import os
 
 def draw(data):
+  # flag = True
   x_value1 = list(range(1, len(data) + 1))
   y_value1 = data
-  title = 'accuracy on cifar10'
+  title = 'Accuracy On CIFAR10'
   plt.style.use = ('seaborn')
   plt.xlim((-1, len(data) + 2))
   plt.ylim(0, 1.1)
@@ -14,21 +15,36 @@ def draw(data):
   ax1.set_title(title, fontsize=20)
   ax1.set_xlabel("epoch", fontsize=10, loc='right')
   ax1.set_ylabel("accuracy",fontsize=10, loc='top')
-  ax1.scatter(x_value1, y_value1, s=4)
-  line1, = ax1.plot(x_value1, y_value1, linewidth=2, linestyle='-')
+  ax1.scatter(x_value1, y_value1, s=3)
+  line1, = ax1.plot(x_value1, y_value1, linewidth=3, linestyle='-')
   fig.legend([line1], ['acc'], loc='center', bbox_to_anchor=(0.8, 0.2))
+
+  # for a, b in zip(x_value1, y_value1):
+  #   if b >= 89 and flag:
+  #     plt.text(a, b, int(a), ha='left', va='top', fontsize=8)
+  #     flag = False
 
   if not os.path.exists('./lab2/figure'):
     os.makedirs('./lab2/figure')
   fig.savefig(f'./lab2/figure/{title}.jpg')
   plt.close()
 
+
+def draw_classes_histogram(data_list):
+  x_labels = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+  y_values = data_list
+  x = range(0, 20, 2)
+  fig, ax = plt.subplots()
+  ax.bar(x, y_values, width=1.4)
+  ax.set_title('Average Accuracy of Each Class', fontsize=20)
+  ax.set_xlabel('labels', loc='right', fontsize=10)
+  ax.set_ylabel('avg_acc', loc='top', fontsize=10)
+  ax.set_xticks(x, x_labels)
+
+  for a, b in zip(x, y_values):
+    plt.text(a, b+0.1, b, ha='center', va='bottom')
+  fig.savefig('./lab2/figure/avg_accs.jpg')
+
+
 if __name__ == '__main__':
-  datalist = [
-    49.03, 56.98, 62.76, 64.50, 67.22, 69.89,
-    71.11, 69.85, 71.21, 75.44, 73.72, 72.55,
-    77.10, 77.75, 79.12, 77.84, 78.46, 77.79,
-    78.46, 77.91, 80.35, 80.41, 81.42, 80.70,
-    81.3
-  ]
-  draw(datalist)
+  False
--- lab2/run_training.py
+++ lab2/run_training.py
@@ -1,16 +1,17 @@
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler
 from torch import optim
 from arch_layers import ResNet
-from draw_fig import draw
+from draw_fig import draw, draw_classes_histogram
 import torch.nn.functional as F
 import numpy as np
 import torch
 import os
 
-MODEL = False
-EPOCHS = 100
+MODEL = True
+EPOCHS = 20
 BATCH_SIZE = 128
 DEVICE = 'cpu'
 
@@ -47,11 +48,10 @@ def test(model, test_loader):
           acc += 1
           correct[prediction[i]] += 1
   ave = acc / 10000
-  print(ave*100, correct)
   return correct, ave*100
 
 
-if __name__ == '__main__':
+def run_training():
   if not os.path.exists('./lab2/dataset'):
     os.makedirs('./lab2/dataset')
   if not os.path.exists('./lab2/model'):
@@ -76,18 +76,47 @@ if __name__ == '__main__':
 
   model = ResNet().to(DEVICE)
   if MODEL:
-    model.load_state_dict(torch.load('./lab2/model/model.pth'))
-  optimizer = optim.Adam(model.parameters())
+    model.load_state_dict(torch.load('./lab2/model/model_245.pth'))
+  optimizer = optim.Adam(model.parameters(), lr=0.0001)
+  scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
   accs = []
   for epoch in range(1, EPOCHS+1):
-    print(f'epoch: {epoch}')
+    # print(f'epoch: {epoch+245}')
     train(model, train_loader, optimizer)
+    scheduler.step()
     correct, acc = test(model, test_loader)
     accs.append(acc)
-    with open(f'./lab2/results_2.txt', "a") as f:
-      f.write(f'epoch{epoch}  ' + f'acc {acc}' + '\n' + str(correct) + '\n')
+    with open(f'./lab2/results_3.txt', "a") as f:
+      f.write(f'epoch{epoch+245}  ' + f'acc {acc}' + '\n' + str(correct) + '\n')
 
     if epoch % 5 == 0:
-      torch.save(model.state_dict(), f'./lab2/model/model_{epoch}.pth')
+      torch.save(model.state_dict(), f'./lab2/model/model_{epoch+245}.pth')
+      with open(f'./lab2/accs.txt', "a") as f:
+        f.write(str(accs) + '\n')
+      accs = []
   draw(accs)
+
+
+if __name__ == '__main__':
+  # run_training()
+  model = ResNet().to(DEVICE)
+  model.load_state_dict(torch.load('./lab2/model/model.pth'))
+  transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+  ])
+  test_dataset = CIFAR10(root='./lab2/dataset', train=False, download=True, transform=transform_test)
+  test_loader = DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
+  accs = []
+  classes_accs = [0.0] * 10
+  for epoch in range(EPOCHS):
+    correct, acc = test(model, test_loader)
+    accs.append(acc)
+    for i in range(10):
+      if epoch:
+        classes_accs[i] = (classes_accs[i] + correct[i]) / 200
+      else:
+        classes_accs[i] = (classes_accs[i] + correct[i]) / 100
+  draw_classes_histogram(classes_accs)
+  print(sum(accs) / len(accs))
